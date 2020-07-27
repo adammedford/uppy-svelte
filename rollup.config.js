ed4ca2b33e3ce2f1213c767ff4b1835fa1b5f392
Adam Medford <adammedford@gmail.com>
2020-07-27 12:09:47 -0500

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+const production = !process.env.ROLLUP_WATCH;
+
+const name = pkg.name
+	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+	.replace(/^\w/, m => m.toUpperCase())
+	.replace(/-\w/g, m => m[1].toUpperCase());
+
+export default {
+	input: 'src/index.js',
+	output: [
+		{ file: pkg.module, 'format': 'es' },
+		{ file: pkg.main, 'format': 'umd', name }
+	],
+	plugins: [
+		svelte({
+			dev: !production,
+			immutable: true,
+			css: false,
+			preprocess: sveltePreprocess(),
+		}),
+		resolve()
+	]
+};
